fe96c60e274cc26ba4fdf8a59439ce24bb865a5e
Disyer <daniel@tohka.us>
2025-05-31 20:50:34 +0300

    Use middleware

--- webp.go
+++ webp.go
@@ -1,161 +1,192 @@
 package webptransform
 
 import (
-    "bytes"
-    "golang.org/x/crypto/blake2b"
-    "encoding/hex"
-    "fmt"
-    "image"
-    "image/jpeg"
-    "image/png"
-    "io"
-    "net/http"
-    "os"
-    "path/filepath"
-    "strings"
-    "sync"
+	"bytes"
+	"encoding/hex"
+	"fmt"
+	"image"
+	"image/jpeg"
+	"image/png"
+	"io"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"sync"
 
-    "github.com/caddyserver/caddy/v2"
-    "github.com/caddyserver/caddy/v2/modules/caddyhttp"
-    "github.com/kolesa-team/go-webp/webp"
-    "github.com/kolesa-team/go-webp/encoder"
+	"golang.org/x/crypto/blake2b"
+
+	"github.com/caddyserver/caddy/v2"
+	"github.com/caddyserver/caddy/v2/caddyconfig/caddyfile"
+	"github.com/caddyserver/caddy/v2/caddyconfig/httpcaddyfile"
+	"github.com/caddyserver/caddy/v2/modules/caddyhttp"
+	"github.com/kolesa-team/go-webp/encoder"
+	"github.com/kolesa-team/go-webp/webp"
 )
 
 func init() {
-    caddy.RegisterModule(WebPTransform{})
+	caddy.RegisterModule(WebPTransform{})
+	httpcaddyfile.RegisterHandlerDirective("webp_transform", parseCaddyfile)
+}
+
+func parseCaddyfile(h httpcaddyfile.Helper) (caddyhttp.MiddlewareHandler, error) {
+	var m WebPTransform
+	err := m.UnmarshalCaddyfile(h.Dispenser)
+	return m, err
 }
 
 type WebPTransform struct {
-    Cache string `json:"cache"`
-    mu    sync.Mutex
+	Cache string `json:"cache"`
+	mu    sync.Mutex
+}
+
+func (m *WebPTransform) UnmarshalCaddyfile(d *caddyfile.Dispenser) error {
+	for d.Next() {
+		for d.NextBlock(0) {
+			switch d.Val() {
+			case "cache":
+				if !d.NextArg() {
+					return d.ArgErr()
+				}
+
+				m.Cache = d.Val()
+			default:
+				return d.Errf("unrecognized directive: %s", d.Val())
+			}
+		}
+	}
+
+	return nil
 }
 
 func (WebPTransform) CaddyModule() caddy.ModuleInfo {
-    return caddy.ModuleInfo{
-        ID:  "http.handlers.webp_transform",
-        New: func() caddy.Module { return new(WebPTransform) },
-    }
+	return caddy.ModuleInfo{
+		ID:  "http.handlers.webp_transform",
+		New: func() caddy.Module { return new(WebPTransform) },
+	}
 }
 
 func (m *WebPTransform) Provision(ctx caddy.Context) error {
-    return nil
+	return nil
 }
 
 func (m *WebPTransform) Validate() error {
-    if m.Cache == "" {
-        return fmt.Errorf("cache directory must be set")
-    }
-    return nil
+	if m.Cache == "" {
+		return fmt.Errorf("cache directory must be set")
+	}
+	return nil
 }
 
-func (m *WebPTransform) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
-    if !strings.Contains(r.Header.Get("Accept"), "image/webp") {
-        return next.ServeHTTP(w, r)
-    }
+func (m WebPTransform) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.Handler) error {
+	if !strings.Contains(r.Header.Get("Accept"), "image/webp") {
+		return next.ServeHTTP(w, r)
+	}
 
-    // Intercept response
-    rw := &interceptResponseWriter{
-        ResponseWriter: w,
-        header:         make(http.Header),
-        buf:            &bytes.Buffer{},
-    }
+	// Intercept response
+	rw := &interceptResponseWriter{
+		ResponseWriter: w,
+		header:         make(http.Header),
+		buf:            &bytes.Buffer{},
+	}
 
-    err := next.ServeHTTP(rw, r)
-    if err != nil {
-        return err
-    }
+	err := next.ServeHTTP(rw, r)
+	if err != nil {
+		return err
+	}
 
-    contentType := rw.header.Get("Content-Type")
-    if !(strings.HasPrefix(contentType, "image/png") || strings.HasPrefix(contentType, "image/jpeg")) {
-        rw.WriteToOriginal(w)
-        return nil
-    }
+	contentType := rw.header.Get("Content-Type")
+	if !(strings.HasPrefix(contentType, "image/png") || strings.HasPrefix(contentType, "image/jpeg")) {
+		rw.WriteToOriginal(w)
+		return nil
+	}
 
-    hashedName := hashPath(r.URL.Path)
-    cachePath := filepath.Join(m.Cache, hashedName+".webp")
+	hashedName := hashPath(r.URL.Path)
+	cachePath := filepath.Join(m.Cache, hashedName+".webp")
 
-    if _, err := os.Stat(cachePath); err == nil {
-        w.Header().Set("Content-Type", "image/webp")
-        http.ServeFile(w, r, cachePath)
-        return nil
-    }
+	if _, err := os.Stat(cachePath); err == nil {
+		w.Header().Set("Content-Type", "image/webp")
+		http.ServeFile(w, r, cachePath)
+		return nil
+	}
 
-    var img image.Image
-    var decodeErr error
-    if strings.HasPrefix(contentType, "image/png") {
-        img, decodeErr = png.Decode(bytes.NewReader(rw.buf.Bytes()))
-    } else {
-        img, decodeErr = jpeg.Decode(bytes.NewReader(rw.buf.Bytes()))
-    }
+	var img image.Image
+	var decodeErr error
 
-    if decodeErr != nil {
-        rw.WriteToOriginal(w)
-        return nil
-    }
+	if strings.HasPrefix(contentType, "image/png") {
+		img, decodeErr = png.Decode(bytes.NewReader(rw.buf.Bytes()))
+	} else {
+		img, decodeErr = jpeg.Decode(bytes.NewReader(rw.buf.Bytes()))
+	}
 
-    options, err := encoder.NewLossyEncoderOptions(encoder.PresetDefault, 75)
-    if err != nil {
-        rw.WriteToOriginal(w)
-        return nil
-    }
+	if decodeErr != nil {
+		rw.WriteToOriginal(w)
+		return nil
+	}
 
-    var buf bytes.Buffer
+	options, err := encoder.NewLossyEncoderOptions(encoder.PresetDefault, 75)
+	if err != nil {
+		rw.WriteToOriginal(w)
+		return nil
+	}
 
-    if err := webp.Encode(&buf, img, options); err != nil {
-        rw.WriteToOriginal(w)
-        return nil
-    }
+	var buf bytes.Buffer
 
-    m.mu.Lock()
-    _ = os.MkdirAll(filepath.Dir(cachePath), 0755)
-    _ = os.WriteFile(cachePath, buf.Bytes(), 0644)
-    m.mu.Unlock()
+	if err := webp.Encode(&buf, img, options); err != nil {
+		rw.WriteToOriginal(w)
+		return nil
+	}
 
-    w.Header().Set("Content-Type", "image/webp")
-    w.Header().Set("Content-Length", fmt.Sprintf("%d", buf.Len()))
-    _, _ = w.Write(buf.Bytes())
-    return nil
+	m.mu.Lock()
+	_ = os.MkdirAll(filepath.Dir(cachePath), 0755)
+	_ = os.WriteFile(cachePath, buf.Bytes(), 0644)
+	m.mu.Unlock()
+
+	w.Header().Set("Content-Type", "image/webp")
+	w.Header().Set("Content-Length", fmt.Sprintf("%d", buf.Len()))
+	_, _ = w.Write(buf.Bytes())
+	return nil
 }
 
 func hashPath(path string) string {
-    h, _ := blake2b.New256(nil)
-    h.Write([]byte(path))
-    return hex.EncodeToString(h.Sum(nil))
+	h, _ := blake2b.New256(nil)
+	h.Write([]byte(path))
+	return hex.EncodeToString(h.Sum(nil))
 }
 
 type interceptResponseWriter struct {
-    http.ResponseWriter
-    header http.Header
-    buf    *bytes.Buffer
-    status int
+	http.ResponseWriter
+	header http.Header
+	buf    *bytes.Buffer
+	status int
 }
 
 func (rw *interceptResponseWriter) Header() http.Header {
-    return rw.header
+	return rw.header
 }
 
 func (rw *interceptResponseWriter) WriteHeader(statusCode int) {
-    rw.status = statusCode
+	rw.status = statusCode
 }
 
 func (rw *interceptResponseWriter) Write(b []byte) (int, error) {
-    return rw.buf.Write(b)
+	return rw.buf.Write(b)
 }
 
 func (rw *interceptResponseWriter) WriteToOriginal(w http.ResponseWriter) {
-    for k, vv := range rw.header {
-        for _, v := range vv {
-            w.Header().Add(k, v)
-        }
-    }
-    if rw.status != 0 {
-        w.WriteHeader(rw.status)
-    }
-    _, _ = io.Copy(w, rw.buf)
+	for k, vv := range rw.header {
+		for _, v := range vv {
+			w.Header().Add(k, v)
+		}
+	}
+	if rw.status != 0 {
+		w.WriteHeader(rw.status)
+	}
+	_, _ = io.Copy(w, rw.buf)
 }
 
 // Interface guards
 var (
-    _ caddy.Module                = (*WebPTransform)(nil)
-    _ caddyhttp.MiddlewareHandler = (*WebPTransform)(nil)
+	_ caddy.Module                = (*WebPTransform)(nil)
+	_ caddyhttp.MiddlewareHandler = (*WebPTransform)(nil)
+	_ caddyfile.Unmarshaler       = (*WebPTransform)(nil)
 )
